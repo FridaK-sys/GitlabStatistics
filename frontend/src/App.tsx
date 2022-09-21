fc761b668155b300e23473062f432cc0de93ea62
Hanna Reiestad <69514432+Hannareie@users.noreply.github.com>
2022-09-21 10:50:13 +0200

    Fix: Update structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Outlet, useLocation, HashRouter } from "react-router-dom";
 import './App.css';
+import Data from './components/Data';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LayoutsWithNavbar />}></Route>
+        <Route path="/Data" element={<Data />} />
+      </Routes>
+    </Router>
   );
-}
+
+  function LayoutsWithNavbar() {
+    return (
+      <div>
+        <Navbar />
+        <main>
+          <Outlet />  
+        </main>
+      </div>
+    );
+  }
+} 
 
 export default App;
