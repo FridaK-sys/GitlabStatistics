ff67cfe0eddb4833e8d82a9c6a7a290a73071827
Juni <junitrigger@gmail.com>
2022-09-26 14:26:44 +0200

    feat: filter closed issues
    Co-authored-by: Frida Karlsen <fridark@stud.ntnu.no>

--- web/src/components/Issues.tsx
+++ web/src/components/Issues.tsx
@@ -1,24 +1,79 @@
+import { Button, Dropdown, Menu, Space, Tooltip, message } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
 import React, { useEffect, useState } from "react";
+
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import { getIssuesFromAPI } from '../api/apiservice';
 import { Issue } from "../api/types";
+import type { MenuProps } from 'antd';
 import { formatDateAndTime } from "./utils";
+import { getIssuesFromAPI } from '../api/apiservice';
 
 export default function Issues() {
 
-    const [issues, setIssues] = useState<Issue[] | null>(null);
+    const [filteredIssues, setFilteredIssues] = useState<Issue[] | null>(null);
 
     useEffect(() => {
         getIssuesFromAPI().then((res) => {
-          if (!res.ok) return console.error(res.status, res.data);
-          setIssues(res.data);
-        });
+            if (!res.ok) return console.error(res.status, res.data);
+            setFilteredIssues(res.data);
+          });
     }, []);
+
+    const handleFilterClick: MenuProps['onClick'] = e => {
+        if (e.key == "3") {
+            getIssuesFromAPI().then((res) => {
+                if (!res.ok) return console.error(res.status, res.data);
+                setFilteredIssues(res.data);
+              });
+        } else if (e.key == "1") {
+            filterClosedIssues();
+        } else {
+            filterOpenIssues();
+        }
+      };
+
+    function filterClosedIssues() {
+        getIssuesFromAPI().then((res) => {
+            if (!res.ok) return console.error(res.status, res.data);
+            setFilteredIssues(res.data.filter(el => /\d/.test(el.closed_at || '')));
+          });
+    }
+
+    function filterOpenIssues() {
+        getIssuesFromAPI().then((res) => {
+            if (!res.ok) return console.error(res.status, res.data);
+            setFilteredIssues(res.data.filter(el => !/\d/.test(el.closed_at || '')));
+          });
+    }
+
+    const menu = (
+    <Menu
+        onClick={handleFilterClick}
+        items={[
+        {
+            label: 'Closed',
+            key: '1',
+            icon: <UserOutlined />,
+        },
+        {
+            label: 'Not closed',
+            key: '2',
+            icon: <UserOutlined />,
+        },
+        {
+            label: 'All',
+            key: '3',
+            icon: <UserOutlined />,
+            }
+        ]}
+    />
+    );
+      
     
     let body: any = []
     
-    issues?.forEach(el => {
+    filteredIssues?.forEach(el => {
         body.push(
             <div className="pb-4">
                 <Card className="me-2">
@@ -45,6 +100,14 @@ export default function Issues() {
     return (
         <Container>                
             <div>
+            <Dropdown overlay={menu}>
+                <Button>
+                    <Space>
+                        Filter
+                    <DownOutlined />
+                    </Space>
+                </Button>
+            </Dropdown>
                 <h3 className="pt-4 pb-4 text-center">Alle issues i prosjektet</h3>
                 <div className="d-flex flex-wrap justify-content-center">
                     {body}
