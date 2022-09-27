6b7d6bbcdbe16e46d93a8538967c7c85097befaf
FridaK-sys <fridark@stud.ntnu.no>
2022-09-27 11:16:14 +0200

    feat: added local storage

--- web/src/components/Homepage.tsx
+++ web/src/components/Homepage.tsx
@@ -1,11 +1,17 @@
 import Container from 'react-bootstrap/Container';
 
 export default function Homepage() {
+    
 
     return(
         <Container>
             <div>
                 <h1>This is the homepage</h1>
+                <h2>Recently viewed</h2>
+                <ul>
+                    {(JSON.parse(localStorage.getItem("repos") || " []")).map((el : String) => <li> {el}</li>)}
+                </ul>
+
             </div>
         </Container>
     );
