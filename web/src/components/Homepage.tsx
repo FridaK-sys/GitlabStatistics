c9ddbf4e988f1d8a9c0d9cc20ad93a9e0cfb0acc
Linn Marie <lmpeders@stud.ntnu.no>
2022-09-30 16:31:41 +0200

    feat: Finished homepage

--- web/src/components/Homepage.tsx
+++ web/src/components/Homepage.tsx
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
+import { Branch, Commit, Project, Issue } from "../api/types";
+import { getAllCommitsFromApi, getIssuesFromAPI, getProjectFromAPI, getAllBranches } from '../api/apiservice';
 
-import { getAllCommitsFromApi } from '../api/apiservice';
-import { Commit } from "../api/types";
-
-import { getIssuesFromAPI } from '../api/apiservice';
-import { Issue } from "../api/types";
-
-import { getProjectFromAPI } from "../api/apiservice";
-import { Project } from "../api/types";
 
 export default function Homepage() {
     
-
-    //contributors, branches (all/active?)
-
+    //Prosjekt-informasjon
     const [project, setProject] = useState<Project | null>(null);
 
     useEffect(() => {
@@ -26,14 +18,9 @@ export default function Homepage() {
 
     const name = project?.name_with_namespace;
 
-    const [commits, setCommits] = useState<Commit[] | null>(null);
 
-    // useEffect(() => {
-    //     getAllCommitsFromApi().then((res) => {
-    //         if (!res.ok) return console.error(res.status, res.data);
-    //         setCommits(res.data);
-    //     });
-    // }, []);
+    //Commit-informasjon
+    const [commits, setCommits] = useState<Commit[] | null>(null);
 
     useEffect(() => {
         getAllCommitsFromApi().then((res) => {
@@ -43,7 +30,7 @@ export default function Homepage() {
 
     const commitNumber = commits?.length;
 
-
+    //Issue-informasjon
     const [allIssues, setAllIssues] = useState<Issue[] | null>(null);
     const [openIssues, setOpenIssues] = useState<Issue[] | null>(null);
     const [closedIssues, setClosedIssues] = useState<Issue[] | null>(null);
@@ -61,6 +48,30 @@ export default function Homepage() {
     const openIssueNumber = openIssues?.length; //Antall åpne issues
     const closedIssueNumber = closedIssues?.length; //Antall lukkede issues
 
+
+    //Branch-informasjon
+    const [branches, setBranches] = useState<Branch[] | null>(null);
+
+    useEffect(() => {
+        getAllBranches().then((res) => {
+            setBranches(res.data);
+        })
+    }, []);
+
+    const branchNumber = branches?.length;
+
+    //Lager en pen liste for å vise frem alle brancher i repoet
+    const body : any = []
+
+    branches?.forEach(el => {
+        body.push(
+            <div>
+                <li>{el.name}
+                </li>
+            </div>
+        )     
+    })
+
     return(
         <Container>
             <div>
@@ -76,7 +87,19 @@ export default function Homepage() {
                     Issues
                 </h3>
                 <p>
-                    There are {issueNumber} number of issues associated to this repository. Of these, {openIssueNumber} are open, and {closedIssueNumber} are closed.
+                    There are {issueNumber} number of issues associated to this repository. Of these, {openIssueNumber} are open, and {closedIssueNumber} are closed. Go to "Issues" to learn more about them.
+                </p>
+                <h3>
+                    Branches
+                </h3>
+                <p>
+                    This repository has {branchNumber} branches. To see a chart of commits per day in every branch, go to "Chart".
+                </p>
+                <h5>
+                    The branches are:
+                </h5>
+                <p>
+                    {body}
                 </p>
                 <h2>Recently viewed</h2>
                 <ul>
